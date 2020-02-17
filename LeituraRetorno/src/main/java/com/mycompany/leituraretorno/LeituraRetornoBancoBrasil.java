7d4166bdf082ed912a204e82c1eb63d5f7082e21
Thányla Sales <thanylas@gmail.com>
2020-02-17 10:43:08 -0300

    Refatoração de formato de data

--- LeituraRetorno/src/main/java/com/mycompany/leituraretorno/LeituraRetornoBancoBrasil.java
+++ LeituraRetorno/src/main/java/com/mycompany/leituraretorno/LeituraRetornoBancoBrasil.java
@@ -27,7 +27,7 @@ public class LeituraRetornoBancoBrasil implements LeituraRetorno {
                 Boleto boleto = new Boleto();
                 boleto.setId(Integer.parseInt(vetor[0]));
                 boleto.setCodBanco(vetor[1]);
-                boleto.setDataVencimento(LocalDate.parse(vetor[2], FORMATO_DATA_HORA));
+                boleto.setDataVencimento(LocalDate.parse(vetor[2], FORMATO_DATA));
                 boleto.setDataPagamento(LocalDate.parse(vetor[3], FORMATO_DATA).atTime(0, 0));
                 boleto.setCpfCliente(vetor[4]);
                 boleto.setValor(Double.parseDouble(vetor[5]));
